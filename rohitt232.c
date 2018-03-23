5c7838c6488a2c994948ac2c2d8425e5cad41a0f
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-03-23 15:45:34 +0530

    Create rohitt232.c

---
+++ rohitt232.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a,b;
+ char c;
+ float d;
+ scanf("%d %d",&a,&b);
+ scanf("%c",&c);
+ d=0;
+ switch(c)
+ {
+  case'%':
+   d=a%b;
+   break;
+  case'/':
+    d=a/b;
+    break;
+  default:
+    printf("invalid");
+ }
+ printf("%f",d);
+ getch();
+ return 0;
+}
